6e93bb37867d8787361496f96124d9d80b0ac8d1
Yurii <ruban.emp@gmail.com>
2022-02-06 18:55:56 +0200

    Homework 1 (retake)

--- Homework_1/js/script.js
+++ Homework_1/js/script.js
@@ -4,19 +4,9 @@ let volume = Math.PI * Math.pow(radius, 2) * height;
 let S = Math.PI*Math.pow(radius, 2);
 let dashes = `---------------------`;
 
-
 console.log(`**************
 Объем цилиндра с площадью основы S = ${S}, радиусом R = ${radius} и высотой H = ${height} равен:
 ${dashes}
 V = ${volume}
 ${dashes}
 end.`);
-
-// Один в один як в завданні, де значення мають різні кольори й знаходяться на різних рядках
-
-// console.log(`**************`);
-// console.log(`Объем цилиндра с площадью основы S =`, S, `, радиусом R =`, radius, ` и высотой H =`, height,` равен:`);
-// console.log(dashes)
-// console.log(`V =`, volume);
-// console.log(dashes)
-// console.log(`end.`);
